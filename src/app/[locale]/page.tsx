62d53cb46dd0255d2829bbddc8e6d8c06cb799b8
Sergio Demis Lopez <sergioska81@hotmail.com>
2025-05-01 09:34:04 -0600

    Refactor Home component to integrate MainBreadcrumb and CardDemo for improved layout and navigation

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,19 +1,47 @@
-import { Button } from '@/components/ui/button'
+import { CardDemo } from '@/components/dashboard/Notifications'
+import { MainBreadcrumb } from '@/components/breadcrumb/MainBreadcrumb'
 import {useTranslations} from 'next-intl';
+
+
+
+// Define el tipo para un elemento del breadcrumb
+interface BreadcrumbItemType {
+  label: string;
+  href?: string; // Opcional para el último elemento que es solo texto
+}
+const breadcrumbHome: BreadcrumbItemType = {
+  label: 'Inicio',
+  href: '#',
+}
+
+const breadcrumbDashboard: BreadcrumbItemType = {
+  label: 'Dashboard',
+  href: '#',
+}
+
+
+
 
 export default function Home() {
   const t = useTranslations('Home');
   return (
-    <div className="max-w-2xl mx-auto">
-      <h1 className="text-4xl font-bold mb-8">Sistema de Temas con TS</h1>
-      <div className="mt-6 space-y-4">
-        <Button>
-          {t('button1')}
-        </Button>
-        <button className="px-6 py-2 rounded-lg bg-secondary text-secondary-foreground hover:bg-secondary/80">
-          {t('button2')}
-        </button>
+    <div className="w-full mx-auto">
+      <MainBreadcrumb
+        items={[
+          breadcrumbHome,
+          breadcrumbDashboard
+        ]}
+      />
+      <div className="flex flex-row p-5 justify-between">
+        <div className="flex">
+          <h1 className="text-2xl font-bold text-gray-800 dark:text-gray-200">
+            Bienvenido Username!
+          </h1>
+        </div>
+        <div className="flex justify-end">
+          <CardDemo />
+        </div>
       </div>
-    </div>
+  </div>
   );
 }
