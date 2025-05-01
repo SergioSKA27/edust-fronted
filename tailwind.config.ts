52bb6b2d031f72a8cf52654b93d081eb89fd6060
Sergio Demis Lopez <sergioska81@hotmail.com>
2025-05-01 07:17:58 -0600

    Add Tailwind CSS configuration file

---
+++ tailwind.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  darkMode: 'class',
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--text)',
+        primary: {
+          DEFAULT: 'var(--primary)',
+          foreground: 'var(--primary-foreground)'
+        },
+        secondary: {
+          DEFAULT: 'var(--secondary)',
+          foreground: 'var(--secondary-foreground)'
+        },
+        accent: 'var(--accent)'
+      }
+    }
+  }
+};
+
+export default config;
